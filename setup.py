e5476b8726ed32f91f1352ae2e565076a5eeef35
CLannadZSY <zsymidi@gmail.com>
2020-11-10 20:43:26 +0800

    版本迭代1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name="scrapy_middlewares",
-    version="1.0.0",
+    version="1.0.1",
     author="CLannadZSY",
     author_email="zsymidi@gmail.com",
     description="scrapy middleware",
